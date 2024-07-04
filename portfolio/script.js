2badf7f9a4299f520fa0b3806ff6b49ac4e075e3
AmitThakur12345 <thakurwork15@gmail.com>
2024-07-04 22:16:10 +0530

    Create script.js

---
+++ portfolio/script.js
@@ -0,0 +1,9 @@
+document.getElementById(‘contact-form’).addEventListener(‘submit’, function(event) { 
+
+    Event.preventDefault(); 
+
+    Alert(‘Thank you for your message! I will get back to you soon.’); 
+
+}); 
+
+ 
